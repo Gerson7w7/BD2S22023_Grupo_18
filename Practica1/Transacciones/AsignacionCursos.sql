deaf4aed0399ebcd96d984a9240ef90c986b4339
PinD20 <enrique.pinula@gmail.com>
2023-08-27 17:17:59 -0600

    Correción en procedimientos 3 y 4
    Validaciones extra en asignación de cursos y creación de usuarios

--- Practica1/Transacciones/AsignacionCursos.sql
+++ Practica1/Transacciones/AsignacionCursos.sql
@@ -6,15 +6,18 @@ BEGIN
         BEGIN TRANSACTION;
 
         DECLARE @idUser uniqueidentifier;
+        DECLARE @idTutor uniqueidentifier;
         DECLARE @creditosUser INT;
         DECLARE @creditosCurso INT;
         DECLARE @nombreCurso NVARCHAR(MAX);
+        DECLARE @repetidos INT;
 
         SELECT @idUser = Id FROM Usuarios u WHERE u.Email = @email;
         IF (@idUser IS NULL)
             BEGIN
             ROLLBACK TRANSACTION;
             SELECT 'No existe un usuario con el email ingresado.' AS ERROR;
+            INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida, no existe un usuario con el email ingresado.');
             END;
         ELSE
             BEGIN
@@ -23,6 +26,7 @@ BEGIN
                 BEGIN
                 ROLLBACK TRANSACTION;
                 SELECT 'El usuario que se intenta asignar no cuenta con un perfil de estudiante.' AS ERROR;
+                INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida, el usuario que se intenta asignar no cuenta con un perfil de estudiante.');
                 END;
             ELSE
                 BEGIN
@@ -31,32 +35,54 @@ BEGIN
                     BEGIN
                         ROLLBACK TRANSACTION;
                         SELECT 'No existe el curso que se intenta asignar.' AS ERROR;
+                        INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida, no existe el curso que se intenta asignar.');
                     END;
                 ELSE
                     BEGIN
                     IF (@creditosUser >= @creditosCurso)
                         BEGIN
-                        -- Asignación
-                        INSERT INTO CourseAssignment (StudentId, CourseCodCourse)
-                            VALUES (@idUser, @codCourse);
+                        SET @IdTutor = (SELECT TOP 1 TutorId FROM practica1.CourseTutor WHERE CourseCodCourse = @codCourse)
+                        IF (@IdTutor IS NULL)
+                            BEGIN
+                                ROLLBACK TRANSACTION;
+                                SELECT 'Asignación fallida, no existe un tutor asignado al curso.' AS ERROR;
+                                INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida, el curso no cuenta con un tutor asignado.');
+                            END;
+                        ELSE
+                            BEGIN
+                                SET @repetidos = (SELECT COUNT(*) FROM practica1.CourseAssignment WHERE StudentId = @idUser and CourseCodCourse = @codCourse);
+                                IF (@repetidos > 0)
+                                    BEGIN
+                                        ROLLBACK TRANSACTION;
+                                        SELECT 'Asignación fallida, el estudiante ya se encuentra asignado al curso.' AS ERROR;
+                                        INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida, el estudiante ya se encuentra asignado al curso.');
+                                    END;
+                                ELSE
+                                    BEGIN
+                                        -- Asignación
+                                        INSERT INTO CourseAssignment (StudentId, CourseCodCourse)
+                                            VALUES (@idUser, @codCourse);
 
-                        -- Notificación estudiante
-                        INSERT INTO Notification (UserId, Message, Date)
-                            VALUES (@idUser, CONCAT('Usted ha sido asignado al curso ', @codCourse, ' - ', @nombreCurso, ' exitosamente.'), GETDATE());
+                                        -- Notificación estudiante
+                                        INSERT INTO Notification (UserId, Message, Date)
+                                            VALUES (@idUser, CONCAT('Usted ha sido asignado al curso ', @codCourse, ' - ', @nombreCurso, ' exitosamente.'), GETDATE());
 
-                        -- Notificación tutor
-                        INSERT INTO Notification (UserId, Message, Date)
-                            VALUES ((SELECT TOP 1 TutorId FROM practica1.CourseTutor WHERE CourseCodCourse = @codCourse),
-                                CONCAT('El estudiante con ID de usuario: ', @idUser, ' ha sido asignado al curso ', @codCourse, ' - ', @nombreCurso, ' exitosamente.'), GETDATE());
+                                        -- Notificación tutor
+                                        INSERT INTO Notification (UserId, Message, Date)
+                                            -- verificar que exista tutor en 
+                                            VALUES ((SELECT TOP 1 TutorId FROM practica1.CourseTutor WHERE CourseCodCourse = @codCourse),
+                                                CONCAT('El estudiante con ID de usuario: ', @idUser, ' ha sido asignado al curso ', @codCourse, ' - ', @nombreCurso, ' exitosamente.'), GETDATE());
 
-                        COMMIT TRANSACTION;
-
-                        SELECT 'Curso asignado correctamente.' AS MENSAJE;
+                                        COMMIT TRANSACTION;
+                                        SELECT 'Curso asignado correctamente.' AS MENSAJE;
+                                    END;
+                            END;
                         END;
                     ELSE
                         BEGIN
                             ROLLBACK TRANSACTION;
                             SELECT 'El estudiante no cumple con la cantidad de créditos necesarios para asignarse al curso.' AS ERROR;
+                            INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida, el estudiante no cuenta con los créditos necesarios.');
                         END;
                     END;
                 END;
@@ -65,5 +91,6 @@ BEGIN
     BEGIN CATCH
         ROLLBACK TRANSACTION;
         SELECT 'Ha ocurrido un error, no fue posible asignar el curso.' AS ERROR;
+        INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Asignación de curso fallida.');
     END CATCH;    
 END;
--- Practica1/Transacciones/CreacionRoles.sql
+++ Practica1/Transacciones/CreacionRoles.sql
@@ -5,29 +5,31 @@ CREATE PROCEDURE practica1.PR4 ( @roleName NVARCHAR(MAX) ) AS
 BEGIN
     BEGIN TRY
         BEGIN TRANSACTION;
-            IF EXISTS (SELECT 1 FROM Roles r WHERE r.RoleName = @roleName)
-                BEGIN
-                    ROLLBACK TRANSACTION;
-                    SELECT 'El rol que se intenta crear ya existe.' AS ERROR;
-                END;
+            IF @roleName NOT LIKE '%[^a-zA-Z]%'
+            BEGIN
+                IF EXISTS (SELECT 1 FROM Roles r WHERE r.RoleName = @roleName)
+                    BEGIN
+                        ROLLBACK TRANSACTION;
+                        SELECT 'El rol que se intenta crear ya existe.' AS ERROR;
+                        INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Creación de rol fallida, el rol que se intenta crear ya existe.');
+                    END;
+                ELSE
+                    BEGIN
+                        INSERT INTO ROLES (Id, RoleName) VALUES (NEWID(), UPPER(@roleName));
+                        COMMIT TRANSACTION;
+                        SELECT CONCAT('Rol ', UPPER(@roleName), ' creado correctamente.') AS MENSAJE;
+                    END;
+            END;
             ELSE
-                BEGIN
-                    IF (@roleName = 'Student' OR @roleName = 'Tutor')
-                        BEGIN
-                            INSERT INTO ROLES (Id, RoleName) VALUES (NEWID(), UPPER(@roleName));
-                            COMMIT TRANSACTION;
-                            SELECT 'Rol creado correctamente.' AS MENSAJE;
-                        END;
-                    ELSE
-                        BEGIN
-                            ROLLBACK TRANSACTION;
-                            SELECT CONCAT('No es posible crear el rol "', @roleName,
-                                '", los roles válidos son: "Student" y "Tutor".') AS ERROR;
-                        END;
-                END;
+            BEGIN
+                ROLLBACK TRANSACTION;
+                SELECT 'El nombre del rol debe contener únicamente letras.' AS ERROR;
+                INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Creación de rol fallida, el nombre de rol debe contener únicamente letras.');
+            END;
     END TRY
     BEGIN CATCH
         ROLLBACK TRANSACTION;
         SELECT 'Ha ocurrido un error, no fue posible crear el rol.' AS ERROR;
+        INSERT INTO HistoryLog (Date, Description) VALUES (GETDATE(), 'Creación de rol fallida.');
     END CATCH;
 END;
